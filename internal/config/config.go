46a172c1aed877ddd0451d1fc4b86b6a8c9666cc
IvanYaremko <ivan.yaremko@gmail.com>
2024-10-15 20:51:55 +0100

    intital config setup

---
+++ internal/config/config.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	fileName = ".gatorconfig.json"
+)
+
+type Config struct {
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func (c *Config) SetUser(username string) error {
+	c.CurrentUserName = username
+	err := write(*c)
+	if err != nil {
+		return fmt.Errorf("setuser error: %w", err)
+	}
+
+	return nil
+}
+
+func ReadConfig() (Config, error) {
+	filePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, fmt.Errorf("error retrieving home directory: %w", err)
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return Config{}, fmt.Errorf("error retrieving config file: %w", err)
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return Config{}, fmt.Errorf("error io read all of file: %w", err)
+	}
+
+	config := Config{}
+	if err := json.Unmarshal(data, &config); err != nil {
+		return Config{}, fmt.Errorf("error unmarshalling config file: %w", err)
+	}
+
+	return config, nil
+}
+
+func getConfigFilePath() (string, error) {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("error retrieving home directory: %w", err)
+	}
+
+	return fmt.Sprintf("%s/%s", dir, fileName), nil
+}
+
+func write(cfg Config) error {
+	bytes, err := json.Marshal(cfg)
+	if err != nil {
+		return fmt.Errorf("func write marshal: %w", err)
+	}
+	filepath, err := getConfigFilePath()
+	if err != nil {
+		return fmt.Errorf("func write get file path: %w", err)
+	}
+	if err := os.WriteFile(filepath, bytes, 0666); err != nil {
+		return fmt.Errorf("func write file error: %w", err)
+	}
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/IvanYaremko/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		fmt.Println("error")
+	}
+	cfg.SetUser("ivan")
+}
