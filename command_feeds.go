a80a4ea55436be598db3eafbc67f7889e7b2c891
Ivan <ivan.yaremko@gmail.com>
2024-10-26 14:46:52 +0100

    Update command_feeds.go

--- command_feeds.go
+++ command_feeds.go
@@ -12,12 +12,10 @@ func handlerFeeds(s *state, cmd command) error {
 	}
 
 	for _, feed := range feeds {
-
 		user, err := s.db.GetUserByID(context.Background(), feed.UserID)
 		if err != nil {
 			return fmt.Errorf("failed to retrieve user at feeds command: %w", err)
 		}
-
 		fmt.Println("Feed name:", feed.Name)
 		fmt.Println("Feed url:", feed.Url)
 		fmt.Println("Feed created by:", user.Name)
